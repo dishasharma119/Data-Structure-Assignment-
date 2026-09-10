89e36af9ba647edba61731e07e38042db2dbbd2e
dishasharma119 <dishasharma110905@gmail.com>
2026-09-10 19:19:00 +0530

    Question 2
    Given a string, reverse it using STACK. For example “DataStructure” should be output as “erutcurtSataD.”

---
+++ Assignment 03/Assign3Ques2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    char stack[100];
+    int top = -1;
+    cout<<"Enter a string: ";
+    cin>>str;
+    
+    for(int i = 0; i < str.length(); i++)  {
+        top++;
+        stack[top] = str[i];
+    }
+    cout<<"Reversed string: ";
+
+    while(top >= 0) {
+        cout << stack[top];
+        top--;
+    }
+    cout << endl;
+    return 0;
+}
